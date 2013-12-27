34ba581cc4e3d2d8b278b896d7c159d8719a6b09
wjiang <wjiang@codeaurora.org>
2013-12-27 10:08:07 +0800

    SoundRecorder: Fix deleting unsupport wav format when recording in MMS.
    In MMS, it doesn't support wav format recoders.
    Delete wav format when recording in MMS.
    
    Change-Id: If21360b502f3bbae4b54cb2f4a6b2b822c398b11
    CRs-Fixed: 563997

--- src/com/android/soundrecorder/SoundRecorder.java
+++ src/com/android/soundrecorder/SoundRecorder.java
@@ -269,6 +269,8 @@ public class SoundRecorder extends Activity
     private AudioManager mAudioManager;
     private boolean mRecorderStop = false;
 
+    private boolean mWAVSupport = true;
+
     int mAudioSourceType = MediaRecorder.AudioSource.MIC;
     static int sOldCallState = TelephonyManager.CALL_STATE_IDLE;
     WakeLock mWakeLock;
@@ -351,6 +353,7 @@ public class SoundRecorder extends Activity
             if (AUDIO_AMR.equals(s) || AUDIO_3GPP.equals(s) || AUDIO_ANY.equals(s)
                     || ANY_ANY.equals(s)) {
                 mRequestedType = s;
+                mWAVSupport = false;
             } else if (s != null) {
                 // we only support amr and 3gpp formats right now 
                 setResult(RESULT_CANCELED);
@@ -371,6 +374,11 @@ public class SoundRecorder extends Activity
         mRequestedType = mSharedPreferences.getString("requestedType", mRequestedType);
         mFileType = mSharedPreferences.getInt("fileType", mFileType);
         mStoragePath = mSharedPreferences.getString("storagePath", mStoragePath);
+        if (!mWAVSupport && mRequestedType == AUDIO_WAVE_2CH_LPCM) {
+            mRequestedType = AUDIO_AMR;
+            mFileType = 0;
+        }
+
         setContentView(R.layout.main);
         mAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
         mRecorder = new Recorder();
@@ -655,7 +663,9 @@ public class SoundRecorder extends Activity
         if (optionType == SETTING_TYPE_FILE_TYPE) {
             adapter.add(R.string.format_setting_amr_item);
             adapter.add(R.string.format_setting_3gpp_item);
-            adapter.add(R.string.format_setting_wav_item);
+            if (mWAVSupport) {
+                adapter.add(R.string.format_setting_wav_item);
+            }
         } else if (optionType == SETTING_TYPE_STORAGE_LOCATION) {
             adapter.add(R.string.storage_setting_local_item);
             adapter.add(R.string.storage_setting_sdcard_item);
