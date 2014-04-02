2ef5550a4ab71cb3904511325575a7969472380b
wjiang <wjiang@codeaurora.org>
2014-04-02 16:10:38 +0800

    SoundRecorder: Avoid error when record start and stop comes too closely.
    If record start and stop come too closely, internal exception error
    will be threw. Add necessary delay before back key is pressed in
    recording state to avoid recording internal error.
    
    CRs-Fixed: 642195
    Change-Id: Ia24c4a45ce4cfdab05d269ec118dcf48f74beecc

--- src/com/android/soundrecorder/SoundRecorder.java
+++ src/com/android/soundrecorder/SoundRecorder.java
@@ -274,6 +274,7 @@ public class SoundRecorder extends Activity
     static final int SAMPLERATE_AMR_WB = 16000;
     static final int SAMPLERATE_8000 = 8000;
     static final long STOP_WAIT = 300;
+    static final long BACK_KEY_WAIT = 400;
     int mAudioOutputFormat = MediaRecorder.OutputFormat.AMR_WB;
     String mAmrWidebandExtension = ".awb";
     private AudioManager mAudioManager;
@@ -515,7 +516,6 @@ public class SoundRecorder extends Activity
         try{
            Thread.sleep(STOP_WAIT);
         } catch (InterruptedException ex) {
-              Log.e(TAG,"sleep() for stop wait interrupted");
         }
     }
 
@@ -860,6 +860,10 @@ public class SoundRecorder extends Activity
                     mRecorder.stop();
                     break;
                 case Recorder.RECORDING_STATE:
+                    try {
+                        Thread.sleep(BACK_KEY_WAIT);
+                    } catch (InterruptedException ex) {
+                    }
                     mRecorder.stop();
                     saveSampleAndExit(true);
                     break;
