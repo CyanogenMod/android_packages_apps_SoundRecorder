f03f829014666e67e3ffbdc271008d9c6335854d
kaiyiz <kaiyiz@codeaurora.org>
2014-04-14 15:37:31 +0800

    SoundRecorder: Make pointer back to initial position.
    When click the done button or discard button,the mVUMeter
    is not reset to original space which make this bug happen
    
    Add sentence to make mVUMeter reset to original space
    when click done or discard button.
    
    CRs-fixed: 646258
    
    Change-Id: Ic00ea5618887924883889039e95449e51ae217d5

--- src/com/android/soundrecorder/SoundRecorder.java
+++ src/com/android/soundrecorder/SoundRecorder.java
@@ -699,10 +699,12 @@ public class SoundRecorder extends Activity
                 mRecorder.stop();
                 mRecorderProcessed = true;
                 saveSampleAndExit(mExitAfterRecord);
+                mVUMeter.resetAngle();
                 break;
             case R.id.discardButton:
                 mRecorder.delete();
                 mRecorderProcessed = true;
+                mVUMeter.resetAngle();
                 //prompt before exit
                 new AlertDialog.Builder(this)
                     .setTitle(R.string.app_name)
